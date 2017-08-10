8c4900c7a6580ff08d7bbc3c26695f8c8072facc
Johnsen <296240671@qq.com>
2017-08-10 18:57:07 +0800

    ✋ how to use mock

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -1,3 +1,43 @@
-
+// mock 用法
 export default {
+  'GET /api/training-categories': (req, res) => {
+    console.log(req.query);
+    if (req.query && req.query._page === '2') {
+      res.json([
+        {
+          'serialNo': '11',
+          'name': '师傅',
+          'description': '对方感受到'
+        },
+        {
+          'serialNo': '12',
+          'name': '师傅',
+          'description': '对方感受到'
+        }
+      ]);
+    } else {
+      res.json([
+        {
+          'serialNo': '1',
+          'name': '哈哈哈',
+          'description': 'is大家防静电服'
+        }
+      ])
+    }
+  },
+  'GET /api/tiers': (req, res) => {
+    res.json({
+      "success": true,
+      "data": [
+        {
+          "tierId": 1,
+          "levle": "N1"
+        },
+        {
+          "tierId": 2,
+          "levle": "N2"
+        },
+      ]
+    })
+  },
 };
